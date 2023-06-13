ff797640431899d6ec9341e083c586ac4651fd77
SCXIV <Scarlettcoding@gmail.com>
2023-06-13 12:44:19 -0400

    removed old response code, added for loop to go through entries and save to feedback

--- script.py
+++ script.py
@@ -5,16 +5,23 @@ import os
 
 directory = '/data/feedback/'
 files = os.listdir(directory)
+keys = ["title", "name", "date", "feedback"]
 parsed_feedback = []
-keys = [title, name, date, feedback]
+web_url = "null"
 
 def readfile(file):
     with open(directory, file) as f:
         line = f.read().splitlines()
     return line
 
+for file in files:
+    line = readfile(file)
+    parsed_feedback.append(dict(zip(keys, line)))
 
+for feedback in parsed_feedback:
+    response = requests.post(web_url, data=feedback)
+    if response.ok:
+        print("Feedback logged successfully")
+    else:
+        print("Entry failed. Error code: {}".format({response.status.code}))
 
-response = requests.get('https://www.google.com')
-if not response.ok:
-    raise Exception("GET failed with status code {}".format(response.status_code))
