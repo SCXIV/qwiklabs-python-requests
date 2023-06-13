779c2b52588ed61fbf6a32c76db27fdd39a5db5f
SCXIV <Scarlettcoding@gmail.com>
2023-06-13 12:37:40 -0400

    added variables for directory, files, keys, feedback, and functions to read files

--- script.py
+++ script.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 import requests
+import os
+
+directory = '/data/feedback/'
+files = os.listdir(directory)
+parsed_feedback = []
+keys = [title, name, date, feedback]
+
+def readfile(file):
+    with open(directory, file) as f:
+        line = f.read().splitlines()
+    return line
+
+
 
 response = requests.get('https://www.google.com')
 if not response.ok:
